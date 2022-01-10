87bdd51cb6a092cc6ff458434bbc8940b40a0ebb
theparthka <parthka.2005@gmail.com>
2022-01-10 10:20:23 +0530

    Get Request With Out Struct

--- tools/getrequest.go
+++ tools/getrequest.go
@@ -2,6 +2,9 @@ package tools
 
 import (
 	"encoding/json"
+	/* "fmt"
+	"io/ioutil"
+	"log" */
 	"net/http"
 )
 
@@ -9,13 +12,26 @@ func StorRequest(r *http.Request, i interface{}) {
 
 	//r.ParseMultipartForm(2 << 20)
 
+	/* var payload interface{} // The interface where we will save the converted JSON data.
+    buffer, err := ioutil.ReadAll(r.Body)
+    if err != nil {
+        log.Panic(err)
+    }
+    r.Body.Close() // Close this
+    json.Unmarshal(buffer, &payload) // Convert JSON data into interface{} type
+    m := payload.(map[string]interface{})
+
+	fmt.Println(m) */
+	
 	json.NewDecoder(r.Body).Decode(&i)
+	
+	
 
 }
 
 func SpliteLastN(s string, n int) string {
 	if len(s) > n {
-		 return s[len(s)-n:]
+		return s[len(s)-n:]
 	}
 	return s
 }
