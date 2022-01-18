1ec8e0340c18ad8d108962290128ca16a9f99828
theparthka <parthka.2005@gmail.com>
2022-01-18 16:15:29 +0530

    add tools for count total day in month

---
+++ tools/otools.go
@@ -0,0 +1,8 @@
+package tools
+
+import "time"
+
+func daysIn(m time.Month, year int) int {
+	// This is equivalent to time.daysIn(m, year).
+	return time.Date(year, m+1, 0, 0, 0, 0, 0, time.UTC).Day()
+}
