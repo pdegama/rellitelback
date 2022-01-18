8f988f9e6ab57ce6035fc2e997accb26ddbfa8ea
theparthka <parthka.2005@gmail.com>
2022-01-18 17:01:14 +0530

    bug fix

--- tools/otools.go
+++ tools/otools.go
@@ -2,7 +2,7 @@ package tools
 
 import "time"
 
-func daysIn(m time.Month, year int) int {
+func DaysIn(m time.Month, year int) int {
 	// This is equivalent to time.daysIn(m, year).
 	return time.Date(year, m+1, 0, 0, 0, 0, 0, time.UTC).Day()
 }
